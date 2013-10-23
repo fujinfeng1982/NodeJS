67d8bc33f4facee6e0e2aec25c8cc69df14d7592
fujinfeng1982 <fjf_2004@163.com>
2013-10-23 23:49:41 +0800

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ function start(route,handle)
     function onRequest(request,response)
     {
         var pathname=url.parse(request.url).pathname;
-        console.log("Request for "+pathname+" received test");
+        console.log("Request for "+pathname+" received test123");
         route(handle,pathname,response,request);
     }
     http.createServer(onRequest).listen(8888);
