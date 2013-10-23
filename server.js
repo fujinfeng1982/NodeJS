e8d11e0a7f71e84c15672b07fec4bedca136f294
Fred <fujinfeng_m@126.com>
2013-10-23 22:48:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ function start(route,handle)
     function onRequest(request,response)
     {
         var pathname=url.parse(request.url).pathname;
-        console.log("Request for "+pathname+" received");
+        console.log("Request for "+pathname+" received test");
         route(handle,pathname,response,request);
     }
     http.createServer(onRequest).listen(8888);
